b58f02335326e47b40c797e323173b2d049ea6c1
Eber Palma <al221911577@gmail.com>
2022-01-28 14:23:57 -0600

    Database

---
+++ laravel_2022-01-28_142324.sql
@@ -0,0 +1,57 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ laravel /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE laravel;
+
+DROP TABLE IF EXISTS migrations;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS personal_access_tokens;
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS td_alumnos;
+CREATE TABLE `td_alumnos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `matricula` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `genero` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_grupo` int(11) NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foto` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS td_grupos;
+CREATE TABLE `td_grupos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `clave` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- resources/views/lista.blade.php
+++ resources/views/lista.blade.php
@@ -1,40 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('body')
+<div id="tabla_de_alumnos">
     <table>
-        <tr>
-            <th>ID</th>
-            <th>Matricula</th>
-            <th>Nombre</th>
-            <th>Fecha de nacimiento</th>
-            <th>Email</th>
-            <th>Acciones</th>
-        </tr>
-        @if($cantidad > 0)
-        @foreach($alumnos as $alumno)
-            <tr>
-                <td>{{ $alumno->id }}</td>
-                <td>{{ $alumno->matricula }}</td>
-                <td>{{ $alumno->nombre }}</td>
-                <td>{{ $alumno->fecha_nacimiento }}</td>
-                <td>{{ $alumno->email }}</td>
-                <td>
-                    <a href="{{ route('detalle') }}">Ver informacion</a>
-                    <a href="{{ route('borrar2') }}">Eliminar</a>
-                </td>
+        <thead>
+            <tr id="thead">
+                <th>ID</th>
+                <th>Matricula</th>
+                <th>Nombre</th>
+                <th>Fecha de nacimiento</th>
+                <th>Email</th>
+                <th>Acciones</th>
             </tr>
-        @endforeach
-        @else
+        </thead>
+        <tbody>
+            @if($cantidad > 0)
+            @foreach($alumnos as $alumno)
+                <tr>
+                    <td>{{ $alumno->id }}</td>
+                    <td>{{ $alumno->matricula }}</td>
+                    <td>{{ $alumno->nombre }}</td>
+                    <td>{{ $alumno->fecha_nacimiento }}</td>
+                    <td>{{ $alumno->email }}</td>
+                    <td>
+                        <a href="{{ route('detalle') }}">Ver informacion</a>
+                        <a href="{{ route('borrar2') }}">Eliminar</a>
+                    </td>
+                </tr>
+            @endforeach
+            @else
+        </tbody>
             <tr>
                 <td colspan="6" style="text-align:center"><h3>No se encontraron registros</h3></td>
             </tr>
         @endif
     </table>
-</body>
-</html>
+</div>
+@endsection
